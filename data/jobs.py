4cf199f0787456d2fdfe8939f73dd7b3e943bc69
tranhuyphong <tranhuyphong2102@gmail.com>
2026-04-21 13:34:42 +0700

    Create jobs.py

---
+++ data/jobs.py
@@ -0,0 +1,13 @@
+jobs = [
+    {
+        "day": 1,
+        "tasks": [
+            {
+                "description": "Góp vốn 100 triệu tiền mặt",
+                "correct": "Nợ 111 / Có 411",
+                "reward": 20,
+                "penalty": -10
+            }
+        ]
+    }
+]
