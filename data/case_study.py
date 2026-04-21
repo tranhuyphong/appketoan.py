28a8de58a1db09eef663c15aa293363269bbb6e9
tranhuyphong <tranhuyphong2102@gmail.com>
2026-04-21 17:15:46 +0700

    Update case_study.py

--- data/case_study.py
+++ data/case_study.py
@@ -1,18 +1,19 @@
 case_studies = [
-
     {
-        "title": "Công ty ABC - Tháng 1",
-
+        "name": "Tháng 1 - Công ty ABC",
         "transactions": [
-            "Góp vốn 100 triệu tiền mặt",
-            "Mua hàng 20 triệu tiền mặt",
-            "Bán hàng thu tiền 30 triệu"
+            "Mua hàng hóa 10,000 chưa thanh toán",
+            "Bán hàng thu tiền mặt 15,000",
+            "Trả lương nhân viên 5,000",
+            "Mua TSCĐ 20,000",
+            "Thanh toán công nợ 10,000"
         ],
-
         "answers": [
-            "Nợ 111 / Có 411",
-            "Nợ 156 / Có 111",
-            "Nợ 111 / Có 511"
+            "Nợ 156 / Có 331",
+            "Nợ 111 / Có 511",
+            "Nợ 642 / Có 111",
+            "Nợ 211 / Có 111",
+            "Nợ 331 / Có 111"
         ]
     }
 ]
