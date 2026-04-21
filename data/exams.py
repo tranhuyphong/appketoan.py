6865479c54578bb8052bcc41a134531ebfab4ec7
tranhuyphong <tranhuyphong2102@gmail.com>
2026-04-21 15:36:03 +0700

    Create exams.py

---
+++ data/exams.py
@@ -0,0 +1,21 @@
+exams = [
+    {
+        "module": "Cấp độ 1",
+        "questions": [
+            {
+                "question": "Tài sản = ?",
+                "options": ["Chi phí", "Nợ + VCSH", "Doanh thu"],
+                "correct": 1
+            },
+            {
+                "question": "Góp vốn bằng tiền mặt?",
+                "options": [
+                    "Nợ 111 / Có 511",
+                    "Nợ 111 / Có 411",
+                    "Nợ 112 / Có 411"
+                ],
+                "correct": 1
+            }
+        ]
+    }
+]
