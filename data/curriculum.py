60226d36ca524ea78561a7e63d358befef849819
tranhuyphong <tranhuyphong2102@gmail.com>
2026-04-21 11:27:33 +0700

    Update and rename data.py to data/curriculum.py

---
+++ data/curriculum.py
@@ -0,0 +1,19 @@
+curriculum = [
+    {
+        "module_name": "THE APPRENTICE",
+        "units": [
+            {
+                "unit_name": "Phương trình kế toán",
+                "lessons": [
+                    {
+                        "lesson_name": "Cơ bản",
+                        "theory": "Tài sản = Nợ phải trả + VCSH",
+                        "question": "Tài sản = ?",
+                        "options": ["Chi phí", "Nợ + VCSH", "Doanh thu"],
+                        "correct": 1
+                    }
+                ]
+            }
+        ]
+    }
+]
