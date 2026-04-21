e661f3220f1096829715242d70f9f937567f8750
tranhuyphong <tranhuyphong2102@gmail.com>
2026-04-21 19:58:18 +0700

    Create career_tasks.py

---
+++ data/career_tasks.py
@@ -0,0 +1,43 @@
+career_tasks = {
+    "Intern": [
+        {
+            "desc": "Mua văn phòng phẩm 2 triệu tiền mặt",
+            "correct": "Nợ 642 / Có 111",
+            "wrong": ["Nợ 156 / Có 111", "Không ghi nhận"]
+        },
+        {
+            "desc": "Thanh toán tiền điện 3 triệu",
+            "correct": "Nợ 642 / Có 111",
+            "wrong": ["Nợ 211 / Có 111", "Nợ 111 / Có 642"]
+        }
+    ],
+
+    "Junior": [
+        {
+            "desc": "Bán hàng thu tiền 10 triệu",
+            "correct": "Nợ 111 / Có 511",
+            "wrong": ["Nợ 511 / Có 111", "Không ghi nhận doanh thu"]
+        },
+        {
+            "desc": "Nhập kho hàng hóa 50 triệu chưa trả",
+            "correct": "Nợ 156 / Có 331",
+            "wrong": ["Nợ 632 / Có 331", "Nợ 156 / Có 111"]
+        }
+    ],
+
+    "Senior": [
+        {
+            "desc": "Khấu hao tài sản 5 triệu",
+            "correct": "Nợ 642 / Có 214",
+            "wrong": ["Nợ 211 / Có 214", "Không khấu hao"]
+        }
+    ],
+
+    "Manager": [
+        {
+            "desc": "Phát hiện sai lệch báo cáo tài chính",
+            "correct": "Điều chỉnh lại báo cáo",
+            "wrong": ["Bỏ qua", "Che giấu số liệu"]
+        }
+    ]
+}
